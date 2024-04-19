c735355c3218a84de5635aa0b1f9df47952eaf4b
Oleg Schegol <olschegolev@yahoo.com>
2024-04-19 20:48:42 +0500

    aut

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\AuthController::class, 'index']);
 
-    Route::get('register', [\App\Http\Controllers\AuthController::class, 'create'])->name('register.create');
+
+    Route::get('/', [\App\Http\Controllers\AuthController::class, 'create'])->name('register.create');
     Route::post('register', [\App\Http\Controllers\AuthController::class, 'store'])->name('register.store');
     Route::get('login', [\App\Http\Controllers\AuthController::class, 'loginForm'])->name('loginForm');
     Route::post('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
